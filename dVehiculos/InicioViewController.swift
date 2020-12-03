295c8cbfa2eca42dd12fde71a56d13460603b39f
Aula11 <aula11@a_11_09.aulastic2012.ual.es>
2020-12-03 14:22:14 +0100

    incluyendo vistas, inicio y información

---
+++ dVehiculos/InicioViewController.swift
@@ -0,0 +1,38 @@
+//
+//  InicioViewController.swift
+//  dVehiculos
+//
+//  Created by Aula11 on 3/12/20.
+//  Copyright © 2020 Juandi. All rights reserved.
+//
+
+import UIKit
+
+class InicioViewController: UIViewController {
+
+    
+
+    @IBOutlet weak var ImagenUsuarioInicio: UIImageView!
+    @IBOutlet weak var nombreUsuarioInicio: UILabel!
+    @IBOutlet weak var correoUsuarioInicio: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+    //NO ESTAN INCLUIDOS LOS SEGUE PARA EVITAR CONFLICTOS
+     
+     
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
