78797de6f84136add065a496b46aa4f2edfa689f
Alberto Fuentes <alberto@Albertos-Mac.local>
2020-12-29 11:40:09 +0100

    Iniciar sesión y registrarse 100x100 terminado

--- dVehiculos/IniciarSesionViewController.swift
+++ dVehiculos/IniciarSesionViewController.swift
@@ -10,56 +10,68 @@ import UIKit
 
 class IniciarSesionViewController: UIViewController {
 
+    // CONEXIONES
     @IBOutlet weak var nombreUsuario: UITextField!
     @IBOutlet weak var contrasenaUsuario: UITextField!
+    
+    // VARIABLES
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     var usuarios:[Persona]?
     var usuario:Persona?
+    
+    // CONSTRUCTOR INICIAL
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    @IBAction func unwrapInformacion(_ send: UIStoryboardSegue){
-        
-        
-    }
+    // INICIAR SESION
     @IBAction func iniciarSesion(_ sender: UIButton) {
+        
         let nombre = nombreUsuario.text
         let contrasena = contrasenaUsuario.text
         var flag = false
-        do{
-            self.usuarios = try context.fetch(Persona.fetchRequest())
-        }catch{}
-        self.usuarios!.forEach { user in
-            if(user.nombre == nombre){
-                if(user.password == contrasena){
-                    flag = true
-                    usuario = user
+        
+        //Comprueba si los campos no estan vacios
+        if(nombre != ""){
+            do{
+                self.usuarios = try context.fetch(Persona.fetchRequest())
+            }catch{}
+            
+            //Comprobar si el usuario existe
+            self.usuarios!.forEach { user in
+                if(user.nombre == nombre){
+                    if(user.password == contrasena){
+                        flag = true
+                        usuario = user
+                    }
                 }
             }
         }
+        
         if(flag){
-            print("Success")
+            
+            print("Exito al iniciar sesion")
             performSegue(withIdentifier: "paginaInicio", sender: self)
-        }
-        else{
-            print("Failed")
+            
+        }else{
+            
+            print("Fallo al iniciar sesion")
+            // Alerta fallo a la hora de registrarse
+            let alert = UIAlertController(title: "No se pudo iniciar sesion", message: "Ha ocurrido un fallo, es posible que el usuario y/o contraseña no sean correctos. Vuelva a intentarlo.", preferredStyle: .alert)
+            let subview = (alert.view.subviews.first?.subviews.first?.subviews.first!)! as UIView
+            subview.layer.cornerRadius = 5
+            subview.backgroundColor = UIColor(red: (247/255.0), green: (108/255.0), blue: (78/255.0), alpha: 1.0)
+            self.present(alert, animated: true, completion: nil)
+            
+            let duracion = DispatchTime.now() + 5
+            DispatchQueue.main.asyncAfter(deadline: duracion){
+                alert.dismiss(animated: true, completion: nil)
+            }
         }
         
     }
+    
+    //Enviar datos de usuario a inicio
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "paginaInicio"{
             let destino = segue.destination as! InicioViewController
--- dVehiculos/RegistrarseViewController.swift
+++ dVehiculos/RegistrarseViewController.swift
@@ -10,54 +10,84 @@ import UIKit
 
 class RegistrarseViewController: UIViewController {
 
+    // CONEXIONES
     @IBOutlet weak var nombreUsuario: UITextField!
     @IBOutlet weak var emailUsuario: UITextField!
     @IBOutlet weak var contrasenaUsuario: UITextField!
     @IBOutlet weak var recontrasenaUsuario: UITextField!
+    
+    // VARIABLES
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    // CONSTRUCTOR INICIAL
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    
-    @IBAction func unwrapInformacion_Registrarse(_ send: UIStoryboardSegue){
-        
-        
-    }
+    // REGISTRARSE
     @IBAction func registrarse(_ sender: UIButton) {
         let nombre = nombreUsuario.text
         let email = emailUsuario.text
         let contrasena = contrasenaUsuario.text
         let recontrasena = recontrasenaUsuario.text
+        
+        //Comprueba si las contraseñas son las mismas
         if(contrasena == recontrasena){
             let newUsuario = Persona(context: self.context)
+            
             newUsuario.nombre = nombre
             newUsuario.email = email
             newUsuario.password = contrasena
-            newUsuario.imagen_perfil = (UIImage(named: "userImg")?.pngData()! as! NSData)
+            newUsuario.imagen_perfil = (UIImage(named: "userImg")?.pngData()! as NSData?)
+            
             do{
                 try self.context.save()
-                print("registro exitoso")
+                
+                print("Exito al registrarse")
+                // Alerta de registro existoso
+                let alert = UIAlertController(title: "Registrado correctamente", message: "Ya puede iniciar sesión", preferredStyle: .alert)
+                let subview = (alert.view.subviews.first?.subviews.first?.subviews.first!)! as UIView
+                subview.layer.cornerRadius = 5
+                subview.backgroundColor = UIColor(red: (53/255.0), green: (254/255.0), blue: (139/255.0), alpha: 1.0)
+                self.present(alert, animated: true, completion: nil)
+                
+                let duracion = DispatchTime.now() + 5
+                DispatchQueue.main.asyncAfter(deadline: duracion){
+                    alert.dismiss(animated: true, completion: nil)
+                }
+                
                 performSegue(withIdentifier: "iniciarSesion", sender: self)
+            
             }catch{
-                print("Error registro")
+                
+                print("Fallo al registrarse")
+                // Alerta fallo a la hora de registrarse
+                let alert = UIAlertController(title: "No se pudo registrar", message: "Ha ocurrido un fallo, vuelva a intentarlo de nuevo", preferredStyle: .alert)
+                let subview = (alert.view.subviews.first?.subviews.first?.subviews.first!)! as UIView
+                subview.layer.cornerRadius = 5
+                subview.backgroundColor = UIColor(red: (247/255.0), green: (108/255.0), blue: (78/255.0), alpha: 1.0)
+                self.present(alert, animated: true, completion: nil)
+                
+                let duracion = DispatchTime.now() + 5
+                DispatchQueue.main.asyncAfter(deadline: duracion){
+                    alert.dismiss(animated: true, completion: nil)
+                }
             }
         }
         else{
-            print("Contraseñas no iguales")
+            
+            print("Fallo de contraseñas")
+            // Alerta de que las contraseñas no son iguales
+            let alert = UIAlertController(title: "", message: "Verifica la contraseña, no son iguales.", preferredStyle: .alert)
+            let subview = (alert.view.subviews.first?.subviews.first?.subviews.first!)! as UIView
+            subview.layer.cornerRadius = 5
+            subview.backgroundColor = UIColor(red: (247/255.0), green: (108/255.0), blue: (78/255.0), alpha: 1.0)
+            self.present(alert, animated: true, completion: nil)
+            
+            let duracion = DispatchTime.now() + 5
+            DispatchQueue.main.asyncAfter(deadline: duracion){
+                alert.dismiss(animated: true, completion: nil)
+            }
         }
     }
 }
